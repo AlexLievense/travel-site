1165e7ab52ae20607399ee77c1e3c9f14283235a
Alex Lievense <alexlievense@gmail.com>
2018-02-11 12:26:41 +0100

    Finished the header styling. Finished the mobile menu functionality.

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -2,8 +2,11 @@ import $ from 'jquery';
 
 class MobileMenu {
   constructor() {
+    this.siteHeader = $('.site-header');
     this.menuIcon = $('.site-header__menu-icon');
     this.menuContent = $('.site-header__menu-content');
+    this.title = $('.large-hero__title');
+    this.subtitle = $('.large-hero__subtitle');
     this.events();
   }
 
@@ -13,6 +16,10 @@ class MobileMenu {
 
   toggleTheMenu(){
     this.menuContent.toggleClass("site-header__menu-content--is-visible");
+    this.siteHeader.toggleClass("site-header__is-expanded");
+    this.menuIcon.toggleClass("site-header__menu-icon--close-x");
+    this.title.toggleClass("large-hero__title-invisible");
+    this.subtitle.toggleClass("large-hero__subtitle-invisible");
   }
 }
 
--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -103,8 +103,11 @@ var MobileMenu = function () {
   function MobileMenu() {
     _classCallCheck(this, MobileMenu);
 
+    this.siteHeader = (0, _jquery2.default)('.site-header');
     this.menuIcon = (0, _jquery2.default)('.site-header__menu-icon');
     this.menuContent = (0, _jquery2.default)('.site-header__menu-content');
+    this.title = (0, _jquery2.default)('.large-hero__title');
+    this.subtitle = (0, _jquery2.default)('.large-hero__subtitle');
     this.events();
   }
 
@@ -117,6 +120,10 @@ var MobileMenu = function () {
     key: 'toggleTheMenu',
     value: function toggleTheMenu() {
       this.menuContent.toggleClass("site-header__menu-content--is-visible");
+      this.siteHeader.toggleClass("site-header__is-expanded");
+      this.menuIcon.toggleClass("site-header__menu-icon--close-x");
+      this.title.toggleClass("large-hero__title-invisible");
+      this.subtitle.toggleClass("large-hero__subtitle-invisible");
     }
   }]);
 
